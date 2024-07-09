0dfda8d8de9e18d27f17a8d1ec266e54c9e7b360
Jeremiah Ang (SP) <jeremiah_ang@sp.edu.sg>
2024-07-09 16:09:57 +0800

    Update border title

--- StudentListPanel.java
+++ StudentListPanel.java
@@ -4,18 +4,21 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
 
 public class StudentListPanel {
     private StudentManager studentManager;
     private JPanel panel;
     private JPanel contentPanel;
     private JButton refreshButton;
+    private TitledBorder titleBorder;
 
     public StudentListPanel(StudentManager studentManager) {
         this.studentManager = studentManager;
         this.panel = new JPanel();
         this.panel.setLayout(new BoxLayout(this.panel, BoxLayout.Y_AXIS));
-        this.panel.setBorder(BorderFactory.createTitledBorder("List"));
+        titleBorder = BorderFactory.createTitledBorder("List");
+        this.panel.setBorder(titleBorder);
 
         this.contentPanel = new JPanel();
         this.contentPanel.setLayout(new BoxLayout(this.contentPanel, BoxLayout.Y_AXIS));
@@ -44,6 +47,9 @@ public class StudentListPanel {
         for (Student student : students) {
             this.contentPanel.add(new JLabel(student.toString()));
         }
+
+        this.titleBorder.setTitle("Hello " + students.length);
+        this.panel.repaint();
 
         this.contentPanel.revalidate();
         this.contentPanel.repaint();
